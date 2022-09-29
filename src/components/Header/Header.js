90402fe1d5bf6724a5346125f3f2bafc50328f6d
mehedi556 <mehe556di@gmail.com>
2022-09-29 21:31:09 +0600

    adding nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,9 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            
+            <img src={logo} alt="" />
+            <h1 className='text-5xl font-bold pl-6 text-black'>UNIT-13</h1>
+
         </div>
     );
 };
