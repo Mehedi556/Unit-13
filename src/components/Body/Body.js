4e76b4c31a680088939f9f7bb588a58b4dcf6d86
mehedi556 <mehe556di@gmail.com>
2022-09-29 21:32:57 +0600

    adding cart container

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -23,7 +23,11 @@ const Body = () => {
 
   return (
     <div className="body block md:flex gap-5">
-      
+      <div className="left-side w-4/4 md:w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {cart.map(cart => (
+          <Cart cart={cart} key={cart.id} handleAddToList={handleAddToList}></Cart>
+        ))}
+      </div>
 
       <div className="right-side w-4/4 md:w-2/4 lg:w-1/4 bg-slate-100 rounded-lg justify-center">
        <SingleCart workout={workout}></SingleCart>
