e69f1508d1a3586e20a2f10c2eb7f42ba96883cd
mehedi556 <mehe556di@gmail.com>
2022-09-29 21:39:20 +0600

    adding single card

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,13 @@ const Cart = ({cart , handleAddToList}) => {
     return (
         <div className='border border-solid border-slate-300 rounded-xl p-3'>
            
-                
+           <img className='rounded-lg' src={picture} alt="" />
+                <div className='text-left py-3 pb-6'>
+                <h1><span className='font-bold'>Name: </span>{name}</h1>
+                <h1><span className='font-bold'>Time required: </span>{Time}s</h1>
+                </div>
+                <button onClick={() => handleAddToList(cart)} className="btn btn-ghost btn-outline w-[100%] justify-center">Add to list</button>
+
             
         </div>
     );
