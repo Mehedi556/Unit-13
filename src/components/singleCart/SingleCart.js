fd784a23d3190b7c26846a97abeb5a43f6c8b03b
mehedi556 <mehe556di@gmail.com>
2022-09-29 21:41:13 +0600

    adding profile in cart

--- src/components/singleCart/SingleCart.js
+++ src/components/singleCart/SingleCart.js
@@ -15,7 +15,13 @@ const handleAddNumber = (num) => {
     return (
         <div>
        
-       
+       <div className='flex m-5 '>
+        <img className='w-[80px] rounded-[50px] m-5' src={Male} alt="" />
+        <div className='mt-7'>
+            <h1 className='font-bold text-lg'>Zahid Hossain</h1>
+            <p className='text-sm'>Sydney, Australia</p>
+        </div>
+       </div>
 
        
 
